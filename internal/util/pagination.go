156e9d506ff528d5269643c1a921c48c7444fc0c
Pradana <nextarituenak01@gmail.com>
2024-02-18 16:04:59 +0000

    feat: pagination set

---
+++ internal/util/pagination.go
@@ -0,0 +1,10 @@
+package util
+
+import "gorm.io/gorm"
+
+func PaginationSet(page int, perPage int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		offset := (page - 1) * perPage
+		return db.Offset(offset).Limit(perPage)
+	}
+}
