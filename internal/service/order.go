50c092da80a622250eba319e335e883c7d4697a1
Pradana <nextarituenak01@gmail.com>
2024-02-19 04:06:40 +0000

    chore: add find by uuid order service

--- internal/service/order.go
+++ internal/service/order.go
@@ -8,6 +8,7 @@ import (
 
 type OrderService interface {
 	Create(req domain.Order) (*web.OrderResponse, error)
+	FindID(uuid string) (*web.OrderResponse, error)
 }
 
 type order struct {
@@ -24,6 +25,25 @@ func (o *order) Create(req domain.Order) (*web.OrderResponse, error) {
 	data, err := o.repo.Create(req)
 	if err != nil {
 		return nil, web.InternalServerError(err.Error())
+	}
+
+	response := web.OrderResponse{
+		UUID:        data.UUID,
+		Username:    data.User.Name,
+		Name:        data.Product.Name,
+		Price:       data.Product.Price,
+		Discount:    data.Product.Discount,
+		Description: data.Product.Description,
+		ExpireAt:    data.Product.CreatedAt,
+	}
+
+	return &response, nil
+}
+
+func (o *order) FindID(uuid string) (*web.OrderResponse, error) {
+	data, err := o.repo.FindID(uuid)
+	if err != nil {
+		return nil, web.StatusNotFound(err.Error())
 	}
 
 	response := web.OrderResponse{
