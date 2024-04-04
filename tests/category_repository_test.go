3a12217d77250296e8795d5326a99c7c346c17af
Pradana <nextarituenak01@gmail.com>
2024-04-04 12:29:15 +0000

    feat: category unit test

--- tests/category_repository_test.go
+++ tests/category_repository_test.go
@@ -1,7 +1,11 @@
 package tests
 
 import (
+	"testing"
+
+	"github.com/rulanugrh/lysithea/internal/entity/domain"
 	repomocks "github.com/rulanugrh/lysithea/internal/mocks"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -15,5 +19,27 @@ func NewCategoryTest() *CategoryTest {
 }
 
 func (category *CategoryTest) TestCategoryCreate() {
+	categoryResult := func(input domain.Category) *domain.Category {
+		output := &domain.Category{}
+		output.Name = input.Name
+		output.Description = input.Description
+		return output
+	}
 
+	category.repo.On("Create", mock.MatchedBy(func(input domain.Category) bool {
+		return input.Name != "" && input.Description != ""
+	})).Return(categoryResult, nil)
+
+	data, err := category.repo.Create(domain.Category{
+		Name:        "Electronic",
+		Description: "Simple Electronic",
+	})
+
+	category.Nil(err)
+	category.Equal("Electronic", data.Name)
+	category.Equal("Simple Electronic", data.Description)
+}
+
+func TestCategory(t *testing.T) {
+	suite.Run(t, NewCategoryTest())
 }
