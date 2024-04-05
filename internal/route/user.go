de9bd5f5852b981924047efc040d9d069736ad80
rulanugrh <nextarituenak01@gmail.com>
2024-04-05 20:10:35 +0700

    feat: edit for handler docs

--- internal/route/user.go
+++ internal/route/user.go
@@ -1,18 +1,15 @@
 package route
 
 import (
-	"fmt"
-
 	"github.com/gorilla/mux"
+	_ "github.com/rulanugrh/lysithea/docs"
 	"github.com/rulanugrh/lysithea/internal/config"
 	handler "github.com/rulanugrh/lysithea/internal/http"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
 func UserRouter(router *mux.Router, handler handler.UserHandler, conf *config.App) {
-	router.HandleFunc("/docs/*", httpSwagger.Handler(
-		httpSwagger.URL(fmt.Sprintf("http://%s:%s/docs/swagger.json", conf.Server.Host, conf.Server.Port)),
-	))
+	router.PathPrefix("/docs").Handler(httpSwagger.WrapHandler)
 	subrouter := router.PathPrefix("/api/v1/user/").Subrouter()
 	subrouter.HandleFunc("/register", handler.Register).Methods("POST")
 	subrouter.HandleFunc("/login", handler.Login).Methods("POST")
